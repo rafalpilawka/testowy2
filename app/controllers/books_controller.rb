c3cca6067ce5f99a4ac51f5a502c64ab7c5d16ff
Bartek Bubacz <bartosz.bubacz@polcode.pl>
2015-01-22 21:00:52 +0100

    Fix for multiple select in books

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -14,7 +14,7 @@ class BooksController < ApplicationController
 	def create
 	 	@book = Book.create(book_params)
 	 	if @book.valid?
-	 		redirect_to books_url ,  :notice=>'utworzono poprawnie' 
+	 		redirect_to books_url ,  :notice=>'utworzono poprawnie'
 	 	else
 	 		render "new"
 	 	end
@@ -41,6 +41,6 @@ class BooksController < ApplicationController
 	private
 
 	def book_params
-		params.require(:book).permit(:title , :pages , :label_ids , :author_id)
+		params.require(:book).permit(:title , :pages , :author_id, :label_ids => [])
 	end
 end
